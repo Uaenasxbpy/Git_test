30ff5f78511c8938719f62fc6b82127f66c80262
Uaenasxbpy <3251316564@qq.com>
2023-10-14 23:32:42 +0800

    一些基础的JavaScript代码

---
+++ images/1693612164190.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <el-form ref="elForm" :model="formData" :rules="rules" size="medium" label-width="100px">
+      <el-form-item label="上传" prop="field104" required>
+        <el-upload ref="field104" :file-list="field104fileList" :action="field104Action"
+          :before-upload="field104BeforeUpload">
+          <el-button size="small" type="primary" icon="el-icon-upload">点击上传</el-button>
+        </el-upload>
+      </el-form-item>
+      <el-form-item size="large">
+        <el-button type="primary" @click="submitForm">提交</el-button>
+        <el-button @click="resetForm">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  props: [],
+  data() {
+    return {
+      formData: {
+        field104: null,
+      },
+      rules: {},
+      field104Action: 'https://jsonplaceholder.typicode.com/posts/',
+      field104fileList: [],
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    submitForm() {
+      this.$refs['elForm'].validate(valid => {
+        if (!valid) return
+        // TODO 提交表单
+      })
+    },
+    resetForm() {
+      this.$refs['elForm'].resetFields()
+    },
+    field104BeforeUpload(file) {
+      let isRightSize = file.size / 1024 / 1024 < 2
+      if (!isRightSize) {
+        this.$message.error('文件大小超过 2MB')
+      }
+      return isRightSize
+    },
+  }
+}
+
+</script>
+<style>
+.el-upload__tip {
+  line-height: 1.2;
+}
+
+</style>
